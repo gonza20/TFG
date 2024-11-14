19f4cbfa9f23f593ec2c9b34b09d4b12d9023f5b
gonza20 <50601142+gonza20@users.noreply.github.com>
2024-11-14 18:32:25 -0300

    Add files via upload

---
+++ tfg-backend/src/test/java/proyecto/desafios/tfg_backend/TfgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.desafios.tfg_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
